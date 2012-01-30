31eedb7c7fb64ea4a0727c3e6fa9c80651e4f4c6
Thomas Hewton-Waters <thewton.waters@gmail.com>
2012-01-29 21:59:39 -0700

    Add class Const to hold constants
    Signed-off-by: Thomas Hewton-Waters <thewton.waters@gmail.com>

---
+++ src/Const.java
@@ -0,0 +1,22 @@
+/*
+ * Author: Thomas Hewton-Waters
+ * Created on: January 29, 2012
+ * Last modified: January 29, 2012
+ */
+
+public class Const {
+	/* Server URL */
+	public static final String kServerUrl = "pason1.enel.ucalgary.ca";
+	
+	/* Server Port */
+	public static final int kServerPort = 6130;
+	
+	/* Login team name */
+	public static final String kTeamName = "Thomas Hewton_Waters";
+	
+	/* Login game password */
+	public static final String kGamePassword = "TEST_GAME";
+	
+	/* End all communications with a line separator */
+	public static final String kLineSeparator = "\r\n";
+}
