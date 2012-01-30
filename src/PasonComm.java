02f7844206d2ff61bbafe0f77b0dce6b776af3c5
Thomas Hewton-Waters <thewton.waters@gmail.com>
2012-01-29 22:11:43 -0700

    Add methods to PasonComm for connecting, closing, reading, and writing
    Signed-off-by: Thomas Hewton-Waters <thewton.waters@gmail.com>

--- src/PasonComm.java
+++ src/PasonComm.java
@@ -5,11 +5,53 @@
  */
 
 import java.net.Socket;
-import java.io.BufferedReader;
-import java.io.PrintWriter;
+import java.io.*;
 
 public class PasonComm {
 	private Socket pSocket;
-	private BufferedReader reader;
-	private PrintWriter writer;
+	private BufferedReader pReader;
+	private PrintWriter pWriter;
+	
+	public boolean connect() throws IOException
+	{
+		/* Connect to the game server */
+		this.pSocket = new Socket( Const.kServerUrl, Const.kServerPort );
+		
+		if( this.pSocket.isConnected() ) {
+			/* If connected, create the reader and writer */
+			pReader = new BufferedReader( new InputStreamReader( pSocket.getInputStream() ) );
+			pWriter = new PrintWriter( pSocket.getOutputStream() );
+			
+			return true;
+		}
+		
+		/* Connection failed */
+		return false;
+	}
+	
+	public boolean isConnected()
+	{
+		return pSocket.isConnected();
+	}
+	
+	public boolean close() throws IOException
+	{
+		/* Close the socket, reader, and writer */
+		this.pSocket.close();
+		this.pReader.close();
+		this.pWriter.close();
+		
+		return this.pSocket.isClosed();
+	}
+	
+	public String readLine() throws IOException
+	{
+		return this.pReader.readLine();
+	}
+	
+	public void writeLine( String s )
+	{
+		pWriter.write( s );
+		pWriter.flush();
+	}
 }
